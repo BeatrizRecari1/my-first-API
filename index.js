c891ee6b4fa37c5c3472a283795ff82b39256ac7
beatrizrecari@gmail.com <beatrizrecari@Beatrizs-MacBook-Pro.local>
2023-01-23 12:03:17 -0500

    our first API

---
+++ index.js
@@ -0,0 +1,40 @@
+import express, { response } from "express";
+
+const app = express();
+app.use(express.json())
+
+let instructors = ["Jiho", "Todd"]
+
+app.post("/instructors", (req, res) => {
+    // take an array of new instructors and merge with existing
+    const newInstructors = req.body.instructors
+    instructors = [...instructors, ...newInstructors]
+    res.send(instructors)
+})
+
+
+// now we just need to list some valid requests
+app.get("/test", (request, response) => {
+    console.log("Test Request Made.")
+    response.send("Our API works!")
+})
+
+app.get("/instructors", (request, response) => {
+    response.send(instructors)
+})
+
+app.get("/secure", (request, response) => {
+    // no users are allowed here
+    response.status(401).send("Not Authorized.")
+})
+
+app.post("/students", (req, res) => {
+    // we need to handle the post request (body)
+    const newStudent = req.body
+    console.log(newStudent)
+    res.send(newStudent)
+})
+
+app.listen(3030, () => {
+    console.log("Listening on http://localhost:3030...")
+})
